96aad5a951bec7d5390f3e5b7e33481cd0d29d29
Diogo Mendonça <diogo.cunhamen@gmail.com>
2022-11-10 14:09:03 +0000

    Added More Sprites for Rooms

--- Assets/Scripts/Dungeon Generator/DungeonGenerator.cs
+++ Assets/Scripts/Dungeon Generator/DungeonGenerator.cs
@@ -57,7 +57,7 @@ public class DungeonGenerator : MonoBehaviour{
 	//DEBUG
 	void DebugGenerate() {
 		d = GenerateDungeon(new System.Random().Next());
-		GenerateLevel(0);
+		GenerateLevel(d, 0);
 	}
 
 	public Dungeon GenerateDungeon(int seed) {
@@ -77,8 +77,8 @@ public class DungeonGenerator : MonoBehaviour{
 		return dungeon;
 	}
 
-	Level GenerateLevel(int levelId) {
-		Level level = d.GetLevel(levelId);
+	public Level GenerateLevel(Dungeon dungeon, int levelId) {
+		Level level = dungeon.GetLevel(levelId);
 		System.Random rng = new System.Random(level.levelSeed);
 
 		boundingBoxes = new List<Rect>();
@@ -94,43 +94,56 @@ public class DungeonGenerator : MonoBehaviour{
 		return level;
 	}
 
+	public void DegenerateLevel() {
+		KillAllChildren();
+	}
+
 	Quaternion[] GetRandomizedRotations(System.Random rng) {
 		List<Quaternion> remainingRotations = new List<Quaternion>(rotations);
 		List<Quaternion> pickedRotations = new List<Quaternion>();
+
 		for(int i = 0; i < 4; i++) {
 			Quaternion picked = remainingRotations[rng.Next()%remainingRotations.Count];
 			pickedRotations.Add(picked);
 			remainingRotations.Remove(picked);
 		}
+
 		return pickedRotations.ToArray();
 	}
-	void CheckConnections(RoomPrefab room, Level level) {
-		foreach(RoomPrefab other in level.rooms) {
-			if(other == room) continue;
-			foreach (GameObject exit in room.entrances)
+
+	void CheckConnection(GameObject exit, GameObject entrance) {
+		if(!entrance.activeSelf) return;
+		if((exit.transform.position - entrance.transform.position).sqrMagnitude < entranceDeadzone) {
+			CreateConnection(exit, entrance);
+		}
+	}
+
+	void CheckConnections(RoomPrefab room, RoomPrefab other) {
+		foreach (GameObject exit in room.entrances)
+		{
+			if(!exit.activeSelf) continue;
+			foreach (GameObject entrance in other.entrances)
 			{
-				if(!exit.activeSelf) continue;
-				foreach (GameObject entrance in other.entrances)
-				{
-					if(!entrance.activeSelf) continue;
-					if((exit.transform.position - entrance.transform.position).sqrMagnitude < entranceDeadzone) {
-						CreateConnection(exit, entrance);
-					}
-				}
+				CheckConnection(exit, entrance);
 			}
 		}
 	}
 
-	void CreateConnection(GameObject exit, GameObject entrance) {
-//		exit.GetComponent<SpriteRenderer>().color = Color.blue;
-//		entrance.GetComponent<SpriteRenderer>().color = Color.blue;
+	void CheckConnections(RoomPrefab room, Level level) {
+		foreach(RoomPrefab other in level.rooms) {
+			if(other == room) continue;
+			CheckConnections(room, other);
+		}
+	}
 
+	void CreateConnection(GameObject exit, GameObject entrance) {
 		exit.SetActive(false);
 		entrance.SetActive(false);
 	}
 
 	bool ValidPlacement(Vector3 displacement, Quaternion rotation, RoomPrefab type) {
 		Rect[] bbs = type.GetBoundingBoxes(displacement, rotation);
+
 		foreach (Rect r in boundingBoxes)
 		{
 			foreach (Rect bb in bbs)
@@ -138,6 +151,7 @@ public class DungeonGenerator : MonoBehaviour{
 				if(r.Overlaps(bb)) return false;
 			}
 		}
+
 		return true;
 	}
 
@@ -149,36 +163,43 @@ public class DungeonGenerator : MonoBehaviour{
 		return prefabs[rng.Next() % prefabs.Length];
 	}
 
+	bool AttemptSpawnRoom(System.Random rng, Level level, GameObject exit, int depth, RoomPrefab neighbourType, GameObject possibleNeighbour) {
+		foreach (Quaternion possibleRotation in GetRandomizedRotations(rng))
+		{
+			foreach (GameObject entrance in neighbourType.entrances)
+			{
+				Vector3 displacement = possibleRotation * -entrance.transform.position;
+				displacement += exit.transform.position;
+				if(ValidPlacement(displacement, possibleRotation, neighbourType)) {
+					GenerateRoom(rng.Next(), displacement, possibleRotation, level, possibleNeighbour, depth + 1);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 	void GenerateAtExit(System.Random rng, Level level, GameObject exit, int depth) {
 		for(int i = 0; i < maxRoomSpawnTries; i++) {
 			GameObject possibleNeighbour = GetRoom(rng);
 			RoomPrefab neighbourType = possibleNeighbour.GetComponent<RoomPrefab>();
-			foreach (Quaternion possibleRotation in GetRandomizedRotations(rng))
-			{
-				foreach (GameObject entrance in neighbourType.entrances)
-				{
-					Vector3 displacement = possibleRotation * -entrance.transform.position;
-					displacement += exit.transform.position;
-					if(ValidPlacement(displacement, possibleRotation, neighbourType)) {
-						GenerateRoom(rng.Next(), displacement, possibleRotation, level, possibleNeighbour, depth + 1);
-						return;
-					}
-				}
-			}
+			if(AttemptSpawnRoom(rng, level, exit, depth, neighbourType, possibleNeighbour)) return;
 		}
 	}
 
 
 	void GenerateRoom(int seed, Vector3 position, Quaternion rotation, Level level, GameObject prefab, int depth) {
 		System.Random rng = new System.Random(seed);
-		//GameObject prefab = possibleRooms[rng.Next() % possibleRooms.Length];
+
 		GameObject newRoom = GameObject.Instantiate(prefab, position, rotation);
 		RoomPrefab type = newRoom.GetComponent<RoomPrefab>();
 		level.AddRoom(type);
 		newRoom.transform.parent = this.transform;
 		boundingBoxes.AddRange(type.GetBoundingBoxes(position, rotation));
+
 		if(onceRoomsRemaining.Contains(prefab)) onceRoomsRemaining.Remove(prefab);
-		if(depth == maxDepth) return;
+		if(bossRoomsRemaining.Contains(prefab)) bossRoomsRemaining.Remove(prefab);
+		if(depth >= maxDepth) return;
 
 		foreach (GameObject exit in type.entrances)
 		{
@@ -188,30 +209,21 @@ public class DungeonGenerator : MonoBehaviour{
 
 	void GenerateBossRoom(Level level) {
 		System.Random rng = new System.Random(level.levelSeed);
+
 		if(bossRoomsRemaining.Count == 0) {
 			Debug.LogWarning("No Boss Rooms Remain to Be Placed! Reseting List");
 			bossRoomsRemaining = new List<GameObject>(bossRooms);
 		}
+
 		GameObject bossRoom = bossRoomsRemaining[rng.Next()%bossRoomsRemaining.Count];
 		RoomPrefab bossRoomType = bossRoom.GetComponent<RoomPrefab>();
-		foreach(GameObject entrance in bossRoomType.entrances) {
-			foreach (RoomPrefab room in level.rooms)
+		
+		foreach (RoomPrefab room in level.rooms)
+		{
+			foreach (GameObject exit in room.entrances)
 			{
-				foreach (GameObject exit in room.entrances)
-				{
-					foreach (Quaternion possibleRotation in GetRandomizedRotations(rng)) {
-						Vector3 displacement = possibleRotation * -entrance.transform.position;
-						displacement += exit.transform.position;
-						if(ValidPlacement(displacement, possibleRotation, bossRoomType)) {
-							GameObject newRoom = GameObject.Instantiate(bossRoom, displacement, possibleRotation);
-							RoomPrefab type = newRoom.GetComponent<RoomPrefab>();
-							level.AddRoom(type);
-							newRoom.transform.parent = this.transform;
-							boundingBoxes.AddRange(type.GetBoundingBoxes(displacement, possibleRotation));
-							bossRoomsRemaining.Remove(bossRoom);
-							return;
-						}
-					}
+				if(AttemptSpawnRoom(rng, level, exit, 200, bossRoomType, bossRoom)) {
+					return;
 				}
 			}
 		}
