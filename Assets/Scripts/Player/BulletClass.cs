4a6c14c3b1be8e125ce65601505a5474b45df5a1
JoseCutileiro <jos3ndo@gmail.com>
2022-11-19 17:33:46 +0000

    fixed: inativo -> ataque

--- Assets/Scripts/Player/BulletClass.cs
+++ Assets/Scripts/Player/BulletClass.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class BulletClass : MonoBehaviour
 {
+    
+    public float DMG_DEAL_MULTIPLIER;
+    public int DURATION = 100;
+    public float SPEED = 0;
+
+    private Vector2 direction;
+    public Rigidbody2D rb;
+    public Animator animator;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemieClass player = other.GetComponent<EnemieClass>();
+        if (player != null)
+        {
+            player.HP -= 1;
+        }
+    }
+    
+    public void Criator(float dmg,int duration, Vector2 dir, float _speed)
+    {
+        DMG_DEAL_MULTIPLIER = dmg;
+        DURATION = duration;
+        direction = dir;
+        SPEED = _speed;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
