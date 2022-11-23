2374fedc64c496f51cd6dc02a86a6f8ba6563ef0
Diogo Mendonça <diogo.cunhamen@gmail.com>
2022-11-23 16:45:18 +0000

    Added FastAndLoose Item

---
+++ Assets/Scripts/Obtainables/Items/FastAndLoose.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FastAndLoose", menuName = "Items/Fast And Loose", order = 0)]
+public class FastAndLoose : Item
+{
+	public float movSpeedIncrease;
+	public float fireRateIncrease;
+    public override void OnPickUp()
+    {
+        PlayerClass.instance.MOV_SPEED += movSpeedIncrease;
+        PlayerClass.instance.FIRE_RATE_MULTIPLIER += fireRateIncrease;
+    }
+}
