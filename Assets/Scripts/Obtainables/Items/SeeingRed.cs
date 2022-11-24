89d5460360419f3b969b329b080863aa955141d1
Diogo Mendonça <diogo.cunhamen@gmail.com>
2022-11-24 16:18:16 +0000

    Added SeeingRed Item

---
+++ Assets/Scripts/Obtainables/Items/SeeingRed.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SeeingRed", menuName = "Items/Seeing Red", order = 0)]
+public class SeeingRed : Item
+{
+	public float damageIncrease;
+	public float defeseReduction;
+    public override void OnPickUp()
+    {
+        PlayerClass.instance.DMG_DEAL_MULTIPLIER += damageIncrease;
+		PlayerClass.instance.DMG_RECEIVE_MULTIPLIER *= defeseReduction;
+    }
+}
